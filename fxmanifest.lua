a10adb95e4b1c50dc119e6f006f7aef9f97de76f
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2025-02-22 14:58:51 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version "cerulean"
 game "gta5"
 lua54 'yes'
 
-author "Hel1bestík#0610"
+author "Hel1bestík#0610 & rework by @.vincyxir"
 description "Simple menu for boosting fps"
 version "1.0.0"
 
