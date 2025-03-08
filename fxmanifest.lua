4e6332fb10472dd0d07f0b08ad85777995c6848a
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2025-03-08 17:24:01 +0100

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,17 +1,17 @@
 fx_version "cerulean"
 game "gta5"
-lua54 'yes'
 
 author "FPSBoost-MENU"
 description "menu for boosting fps"
 version "1.0.0"
 
-client_scripts {
-        'client/client.lua',
+shared_scripts {
+        '@ox_lib/init.lua',
+	'config.lua'
 }
 
-shared_scripts {
-        '@ox_lib/init.lua'
+client_scripts {
+        'client/client.lua',
 }
 
 dependencies {
