178e68f417f824a72835a6fad5b2d7194db304f4
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2025-03-10 19:09:35 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,6 +2,6 @@ Config = {}
 
 Config.FeatureExperimental = false -- If you set this to True, you will enable experimental features
 
-config.fpstimecycle_test = false
+Config.fpstimecycle_test = false
 
-config.clearpedandobj = true
+Config.clearpedandobj = true
