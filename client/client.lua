1ed7cc06b9e0c6de914cce07d780d7486c5191f2
vincyxiroff <88449249+vincyxiroff@users.noreply.github.com>
2025-03-12 21:39:40 +0100

    test

--- client/client.lua
+++ client/client.lua
@@ -1,20 +1,111 @@
 local ESX = exports['es_extended']:getSharedObject()
 local ox_lib = exports.ox_lib
+local XPedId = PlayerPedId()
+local XPedCords = GetEntityCoords(XPedId)
+
+local entityEnumerator = {
+    __gc = function(enum)
+        if enum.destructor and enum.handle then
+            enum.destructor(enum.handle)
+        end
+        enum.destructor = nil
+        enum.handle = nil
+    end
+}
+
+local function EnumerateEntities(initFunc, moveFunc, disposeFunc)
+    return coroutine.wrap(
+        function()
+            local iter, id = initFunc()
+            if not id or id == 0 then
+                disposeFunc(iter)
+                return
+            end
+
+            local enum = {handle = iter, destructor = disposeFunc}
+            setmetatable(enum, entityEnumerator)
+
+            local next = true
+            repeat
+                coroutine.yield(id)
+                next, id = moveFunc(iter)
+            until not next
+
+            enum.destructor, enum.handle = nil, nil
+            disposeFunc(iter)
+        end
+    )
+end
+
+local function GetWorldObjects()
+    return EnumerateEntities(FindFirstObject, FindNextObject, EndFindObject)
+end
+
+local function GetWorldPeds()
+    return EnumerateEntities(FindFirstPed, FindNextPed, EndFindPed)
+end
+
+local function GetWorldVehicles()
+    return EnumerateEntities(FindFirstVehicle, FindNextVehicle, EndFindVehicle)
+end
+
+local function modifyWorldObjects(alpha, distance)
+    for obj in GetWorldObjects() do
+        if not IsEntityOnScreen(obj) and #(XPedCords - GetEntityCoords(obj)) > distance then
+            SetEntityAlpha(obj, 0)
+            if not IsEntityAMissionEntity(obj) then
+                SetEntityAsNoLongerNeeded(obj)
+            end
+        else
+            if GetEntityAlpha(obj) ~= alpha then
+                SetEntityAlpha(obj, alpha)
+            end
+        end
+        Wait(0)
+    end
+end
+
+local function modifyWorldPeds(alpha, distance)
+    for obj in GetWorldPeds() do
+        if not IsEntityOnScreen(obj) and #(XPedCords - GetEntityCoords(obj)) > distance then
+            SetEntityAlpha(obj, 0)
+            if not IsEntityAMissionEntity(obj) then
+                SetEntityAsNoLongerNeeded(obj)
+            end
+        else
+            if GetEntityAlpha(obj) ~= alpha then
+                SetEntityAlpha(obj, alpha)
+            end
+        end
+        SetPedAoBlobRendering(obj, false)
+        Wait(0)
+    end
+end
+
+local function modifyWorldVehicles(alpha, distance)
+    for obj in GetWorldVehicles() do
+        if not IsEntityOnScreen(obj) and #(XPedCords - GetEntityCoords(obj)) > distance then
+            SetEntityAlpha(obj, 0)
+            if not IsEntityAMissionEntity(obj) then
+                SetEntityAsNoLongerNeeded(obj)
+            end
+        else
+            if GetEntityAlpha(obj) ~= alpha then
+                SetEntityAlpha(obj, alpha)
+            end
+        end
+        Wait(0)
+    end
+end
 
 local function applyFPSBoost(level)
     if level == "ultra_low" then
-        if Config.fpstimecycle_test then
-            SetTimecycleModifier('HicksbarNEW')
-        end
         SetTimecycleModifier("yell_tunnel_nodirect")
-        SetTimecycleModifierStrength(1.0)
-        SetArtificialLightsState(false)
-        SetLightsCutoffDistanceTweak(5.0)
-        SetFlashLightKeepOnWhileMoving(false)
-        SetReducePedModelBudget(true)
-        SetReduceVehicleModelBudget(true)
-        SetParticleFxNonLoopedAlpha(0.0)
-        SetWeatherTypePersist("CLEAR")
+        modifyWorldObjects(210,90)
+        modifyWorldPeds(245, 60)
+        modifyWorldVehicles(255, 120)
+        DisableOcclusionThisFrame()
+        SetDisableDecalRenderingThisFrame()
         RopeDrawShadowEnabled(false)
         CascadeShadowsClearShadowSampleType()
         CascadeShadowsSetAircraftMode(false)
@@ -26,23 +117,31 @@ local function applyFPSBoost(level)
         SetFlashLightFadeDistance(0.0)
         SetLightsCutoffDistanceTweak(0.0)
         DistantCopCarSirens(false)
-        if Config.FeatureExperimental then
-            print("no Experimental feature")
-        end
+        SetArtificialLightsState(true)
+        RemoveParticleFxInRange(XPedCords, 10.0)
+        OverrideLodscaleThisFrame(0.4)
         fpsBoostActive = true
     elseif level == "medium" then
         SetTimecycleModifier("yell_tunnel_nodirect")
+        modifyWorldObjects(210,130)
+        modifyWorldPeds(250, 100)
+        modifyWorldVehicles(255, 160)
         SetTimecycleModifierStrength(0.5)
         SetArtificialLightsState(false)
         SetReducePedModelBudget(false)
         SetReduceVehicleModelBudget(false)
         SetParticleFxNonLoopedAlpha(0.5)
+        OverrideLodscaleThisFrame(0.6)
         fpsBoostActive = false
         fpsBoostnopedandobj = true
     elseif level == "high" then
         ClearTimecycleModifier()
+        modifyWorldObjects(245,200)
+        modifyWorldPeds(255, 150)
+        modifyWorldVehicles(255, 250)
         SetArtificialLightsState(false)
         SetParticleFxNonLoopedAlpha(1.0)
+        OverrideLodscaleThisFrame(0.8)
         fpsBoostActive = false
         fpsBoostnopedandobj = false
     end
@@ -50,6 +149,7 @@ end
 
 local function setUltraGraphics()
     ClearTimecycleModifier()
+    ClearExtraTimecycleModifier()
     SetArtificialLightsState(false)
     SetParticleFxNonLoopedAlpha(1.0)
     OverrideLodscaleThisFrame(2.0) 
@@ -62,16 +162,35 @@ local function setUltraGraphics()
     RopeDrawShadowEnabled(true)
     SetTimecycleModifier('v_torture')
     SetExtraTimecycleModifier('reflection_correct_ambient')
-    if Config.FeatureExperimental then
-        print("no Experimental feature")
-    end
     fpsBoostActive = false
     fpsBoostnopedandobj = false
 end
 
 
 local function resetSettings()
+    for obj in GetWorldObjects() do
+        if GetEntityAlpha(obj) ~= 255 then
+            SetEntityAlpha(obj, 255)
+        end
+        Wait(0)
+    end
+    for obj in GetWorldPeds() do
+        if GetEntityAlpha(obj) ~= 255 then
+            SetEntityAlpha(obj, 255)
+        end
+        SetPedAoBlobRendering(obj, true)
+        Wait(0)
+    end
+    for obj in GetWorldVehicles() do
+        if GetEntityAlpha(obj) ~= 255 then
+            SetEntityAlpha(obj, 255)
+        end
+        Wait(0)
+    end
+    OverrideLodscaleThisFrame(1.0)
+    DisableVehicleDistantlights(false)
     ClearTimecycleModifier()
+    ClearExtraTimecycleModifier()
     SetArtificialLightsState(false)
     SetParticleFxNonLoopedAlpha(1.0)
     fpsBoostActive = false
@@ -98,6 +217,7 @@ lib.registerContext({
     id = 'fps_boost_menu',
     title = 'FPS Boost Menu',
     options = {
+        { title = 'TimeCycles', description = 'Several Time Cycle', icon = 'audio-description', onSelect = function() TimeCyclesMenuOpen() end},
         { title = 'Ultra Low Boost', description = 'Max FPS Boost', icon = 'rocket', onSelect = function() applyFPSBoost("ultra_low") lib.notify({ title = 'Ultra Low Boost', description = 'Boost FPS applicato.', type = 'success' }) end },
         { title = 'Medium Boost', description = 'Medium Boost', icon = 'tachometer-alt', onSelect = function() applyFPSBoost("medium") lib.notify({ title = 'Medium Boost', description = 'Boost FPS applicato.', type = 'success' }) end },
         { title = 'High Boost', description = 'Low Boost', icon = 'tachometer-alt', onSelect = function() applyFPSBoost("high") lib.notify({ title = 'High Boost', description = 'Boost FPS applicato.', type = 'success' }) end },
@@ -105,6 +225,75 @@ lib.registerContext({
         { title = 'Reset', description = 'Reset', icon = 'undo', onSelect = function() resetSettings() lib.notify({ title = 'Reset', description = 'Impostazioni ripristinate.', type = 'success' }) end }
     }
 })
+
+local TimeCyclesMenuOpen()
+    lib.registerContext({
+        id = 'timecycle_menuu',
+        title = 'TimeCycles Menu',
+        {
+            title = 'Tunnel',
+            description = 'FPS BOOST',
+            icon = 'star',
+            onSelect = function()
+                ClearTimecycleModifier()
+                SetTimecycleModifier("yell_tunnel_nodirect")
+            end,
+        },
+        {
+            title = 'Cinema',
+            description = 'FPS BOOST',
+            icon = 'face-smile',
+            onSelect = function()
+                ClearExtraTimecycleModifier()
+                ClearTimecycleModifier()
+                SetTimecycleModifier("cinema")
+            end,
+        },
+        {
+            title = 'Life',
+            description = 'FPS BOOST',
+            icon = 'web-awesome',
+            onSelect = function()
+                ClearExtraTimecycleModifier()
+                ClearTimecycleModifier()
+                SetTimecycleModifier("LifeInvaderLOD")
+            end,
+        },
+        {
+            title = 'Reduce Distance',
+            description = 'FPS BOOST',
+            icon = 'circle',
+            onSelect = function()
+                ClearExtraTimecycleModifier()
+                ClearTimecycleModifier()
+                SetTimecycleModifier("ReduceDrawDistanceMission")
+            end,
+        },
+        {
+            title = 'PowerPlay Blend',
+            description = 'GRAPHIC',
+            icon = 'circle',
+            onSelect = function()
+                ClearExtraTimecycleModifier()
+                ClearTimecycleModifier()
+                SetTimecycleModifier("MP_Powerplay_blend")
+            end,
+        },
+        {
+            title = 'Tunnel',
+            description = 'Improved light',
+            icon = 'circle',
+            onSelect = function()
+                ClearExtraTimecycleModifier()
+                ClearTimecycleModifier()
+                SetTimecycleModifier("tunnel")
+                SetExtraTimecycleModifier("reflection_correct_ambient")
+            end,
+        }
+
+        lib.showContext('timecycle_menuu')
+    })
+end
 
 RegisterCommand('fps', function()
     lib.showContext('fps_boost_menu')
@@ -115,7 +304,7 @@ AddEventHandler('fpsboost:openMenu', function()
     lib.showContext('fps_boost_menu')
 end)
 
-Citizen.CreateThread(function()
+/*Citizen.CreateThread(function()
     while true do
         if Config.clearpedandobj then
             if fpsBoostActive then
@@ -165,7 +354,7 @@ Citizen.CreateThread(function()
         end
         Citizen.Wait(8)
     end
-end)
+end)*/
 
 Citizen.CreateThread(function()
     while true do
